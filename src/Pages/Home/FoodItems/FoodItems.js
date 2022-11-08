6f0d95fb18b4f6b24b1694639a23a1d2b76ed6c9
Mohammod Ali <mohammodali651@gmail.com>
2022-11-08 22:21:08 +0600

    food items components added

---
+++ src/Pages/Home/FoodItems/FoodItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FoodItems = () => {
+    return (
+        <div>
+            <h1>Food items section</h1>
+        </div>
+    );
+};
+
+export default FoodItems;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import FoodItems from '../FoodItems/FoodItems';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <FoodItems></FoodItems>
         </div>
     );
 };
