bcce14a1035386fcc2e5e867d7a6cb9cc46be845
Mohammod Ali <mohammodali651@gmail.com>
2022-11-09 16:49:14 +0600

    show three component in home page and all component show in services page

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -23,7 +23,7 @@ const Service = () => {
           
            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 my-10'>
                 {
-                    services.map(service=> 
+                    services.slice(0, 3).map(service=> 
         
                         <ServiceCard
                     key={service._id}
--- src/Pages/Home/Service/ServiceCard.js
+++ src/Pages/Home/Service/ServiceCard.js
@@ -12,7 +12,7 @@ const ServiceCard = ({ service}) => {
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
-        <p>{description}</p>
+        <p>{description.slice(0, 100)}{'...'}</p>
         <div className="card-actions justify-between">
         <h4 className="font-bold">Price: ${price}</h4>
           
